95a5d8fac41d42649d43d09fde23e892e3a55ed9
Jesse Robert Finley <jrfinley42@students.tntech.edu>
2017-02-02 10:15:21 -0600

    Fixed space error

--- Password.h
+++ Password.h
@@ -6,15 +6,15 @@
 class Password
 {
 	
-   private:
-      ListArray<String>* viable_words;  //the list of words that can still be the password
-      ListArray<String>* all_words;  //the original list of words
-      int len;  //the length of the first word entered is stored to check that all subsequent words have the same length
+	private:
+		ListArray<String>* viable_words;  //the list of words that can still be the password
+		ListArray<String>* all_words;  //the original list of words
+		int len;  //the length of the first word entered is stored to check that all subsequent words have the same length
 
-      //a private helper method to report the number of character matches between two Strings
-      int getNumMatches(String* curr_word, String* word_guess);
+		//a private helper method to report the number of character matches between two Strings
+		int getNumMatches(String* curr_word, String* word_guess);
 
-   public:
+	public:
 
 
 };
