3d0f45342c25326a80d847625cf78fee889941ff
Dakota Wilkerson <ddwilkerso42@students.tntech.edu>
2017-02-02 10:20:55 -0600

    fix merge

--- Password.h
+++ Password.h
@@ -10,7 +10,6 @@ class Password
 		ListArray<String>* all_words;  //the original list of words
 		int len;  //the length of the first word entered is stored to check that all subsequent words have the same length
 
-
 		//a private helper method to report the number of character matches between two Strings
 		int getNumMatches(String* curr_word, String* word_guess);
 
